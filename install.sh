97b3df3f8bcb95932fdc23cbf6c01c05a18583bd
Victor Santana <victor@hepoca.com>
2018-11-07 19:36:47 -0600

    Updating script

--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ fi
 # Install Node.JS
 # https://github.com/mklement0/n-install
 # curl -L https://git.io/n-install | bash
+# https://github.com/tj/n
+# https://stackoverflow.com/questions/42741243/how-do-you-install-newest-version-of-node-js-on-raspberry-pi
 if [ $(which node 2>/dev/null) ]; then
   echo "nodejs found"
 else
