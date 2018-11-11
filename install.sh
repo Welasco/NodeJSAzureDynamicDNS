8f6bca96d33d58d8b66a6c7b14246ddf12014a74
Victor Santana <victor@hepoca.com>
2018-11-10 20:36:29 -0600

    Updating install.sh and service file

--- install.sh
+++ install.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+INSTALL_DIR=/opt/NodeJSAzureDynamicDNS
+
 #Step 1) Check if root--------------------------------------
 if [[ $EUID -ne 0 ]]; then
    echo "Please execute the installation script as root." 
    exit 1
 fi
 #-----------------------------------------------------------
+install (){
+  if [-d "$INSTALL_DIR"]; then
+    echo "NodeJSAzureDynamicDNS already installed!"
+  else
+    echo "Installing NodeJSAzureDynamicDNS"
+    apt-get update && sudo apt-get install unzip -y
+    wget -Nnv https://github.com/Welasco/NodeJSAzureDynamicDNS/archive/master.zip -O NodeJSAzureDynamicDNS.zip
+    unzip NodeJSAzureDynamicDNS.zip  -d /opt
+    mv /opt/NodeJSAzureDynamicDNS-master/ $INSTALL_DIR
+    chmod +755 $INSTALL_DIR/UpdateAzureRMDDNS.js
+    cd $INSTALL_DIR
+    npm install
+    cp $INSTALL_DIR/UpdateAzureRMDDNS.service /lib/systemd/system
+    systemctl daemon-reload
+    systemctl enable UpdateAzureRMDDNS
+
+    echo "NodeJSAzureDynamicDNS Installed at $INSTALL_DIR."
+    echo "You must setup the $INSTALL_DIR/config.json using your settings!"
+    echo "Start the service after: sudo systemctl start UpdateAzureRMDDNS"
+  fi
+}
 
 # Checking if Node.JS is installed
 # Install Node.JS
@@ -13,22 +36,14 @@ fi
 # curl -L https://git.io/n-install | bash
 # https://github.com/tj/n
 # https://stackoverflow.com/questions/42741243/how-do-you-install-newest-version-of-node-js-on-raspberry-pi
-if [ $(which node 2>/dev/null) ]; then
-  echo "NodeJS found"
+if ! [ -x "$(command -v node)" ]; then
+  echo "NodeJS Not found"
+  echo "Installing NodeJS"
+  curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+  sudo apt-get install -y nodejs  
+  install
 else
-  wget -Nnv https://git.io/n-install && bash n-install; rm -f n-install
-  # curl -L https://git.io/n-install | bash
+  echo "NodeJS already installed"
+  install
 fi
 
-mkdir /opt
-wget -Nnv https://github.com/Welasco/NodeJSAzureDynamicDNS/archive/master.zip -O NodeJSAzureDynamicDNS.zip
-unzip NodeJSAzureDynamicDNS.zip  -d /opt
-mv /opt/NodeJSAzureDynamicDNS-master/ /opt/NodeJSAzureDynamicDNS
-chmod +755 /opt/NodeJSAzureDynamicDNS/UpdateAzureRMDDNS.js
-cp /opt/NodeJSAzureDynamicDNS/UpdateAzureRMDDNS.service /lib/systemd/systemd
-systemctl daemon-reload
-sudo systemctl enable UpdateAzureRMDDNS
-
-echo "NodeJSAzureDynamicDNS Installed at /opt/NodeJSAzureDynamicDNS."
-echo "You must setup the /opt/NodeJSAzureDynamicDNS/config.json using your settings!"
-echo "Start the service after: sudo systemctl start UpdateAzureRMDDNS"
